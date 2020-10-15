ba90bd1021828d5311e1d8e3441ffe96b8af9876
AlanYHung <AlanYHung@hotmail.com>
2020-10-15 12:00:52 -0700

    Adding Lab 7 Modifications

--- greeting.js
+++ greeting.js
@@ -1,19 +1,34 @@
-var today = new Date();
-var CurrHour = today.getHours();
-var dynamicgreeting = "Welcome";
-var UserName = prompt("Please enter your name.")
+function timeofday(){
+    var today = new Date();
+    var CurrHour = today.getHours();
+    var dynamicgreeting = "";
 
-if (CurrHour >= 18)
-{
-    dynamicgreeting = "Good Evening "
-}
-else if (CurrHour >= 12)
-{
-    dynamicgreeting = "Good Afternoon "
-}
-else
-{
-    dynamicgreeting = "Good Morning "
+    if (CurrHour >= 18)
+    {
+        dynamicgreeting = "Good Evening "
+    }
+    else if (CurrHour >= 12)
+    {
+        dynamicgreeting = "Good Afternoon "
+    }
+    else if (CurrHour > 0)
+    {
+        dynamicgreeting = "Good Morning "
+    }
+    else
+    {
+        dynamicgreeting = "Error"
+    }
+
+    return dynamicgreeting;
 }
 
-document.write (dynamicgreeting + UserName + "!")
+function getUserName(){
+    var UserName = prompt("Please enter your name.")
+
+    return UserName;
+}
+
+function greeting(Who, When){
+    return (When + Who + "!")
+}
